986d404b973992421d8edde4014e36ba21d8527f
EVIE Tipsy Doll <126894089+evielm9awda@users.noreply.github.com>
2024-07-10 11:13:52 -0400

    gradient descent and Adam optimization algorithms

--- optimization.c
+++ optimization.c
@@ -28,29 +28,30 @@ void adam_optimizer(NeuralNetwork* nn, double* gradients, double learning_rate, 
         v = (double*)calloc(total_params, sizeof(double));
     }
 
-    ++t;
+    t++;
+
+    int param_idx = 0;
     for (int i = 0; i < nn->num_layers; ++i) {
         Layer* layer = &nn->layers[i];
-        int offset = i * (layer->num_inputs * layer->num_outputs + layer->num_outputs);
-
         for (int j = 0; j < layer->num_inputs * layer->num_outputs; ++j) {
-            m[offset + j] = beta1 * m[offset + j] + (1 - beta1) * gradients[offset + j];
-            v[offset + j] = beta2 * v[offset + j] + (1 - beta2) * gradients[offset + j] * gradients[offset + j];
+            m[param_idx] = beta1 * m[param_idx] + (1.0 - beta1) * gradients[param_idx];
+            v[param_idx] = beta2 * v[param_idx] + (1.0 - beta2) * gradients[param_idx] * gradients[param_idx];
 
-            double m_hat = m[offset + j] / (1 - pow(beta1, t));
-            double v_hat = v[offset + j] / (1 - pow(beta2, t));
+            double m_hat = m[param_idx] / (1.0 - pow(beta1, t));
+            double v_hat = v[param_idx] / (1.0 - pow(beta2, t));
 
             layer->weights[j] -= learning_rate * m_hat / (sqrt(v_hat) + epsilon);
+            param_idx++;
         }
-
         for (int j = 0; j < layer->num_outputs; ++j) {
-            m[offset + layer->num_inputs * layer->num_outputs + j] = beta1 * m[offset + layer->num_inputs * layer->num_outputs + j] + (1 - beta1) * gradients[offset + layer->num_inputs * layer->num_outputs + j];
-            v[offset + layer->num_inputs * layer->num_outputs + j] = beta2 * v[offset + layer->num_inputs * layer->num_outputs + j] + (1 - beta2) * gradients[offset + layer->num_inputs * layer->num_outputs + j] * gradients[offset + layer->num_inputs * layer->num_outputs + j];
+            m[param_idx] = beta1 * m[param_idx] + (1.0 - beta1) * gradients[param_idx];
+            v[param_idx] = beta2 * v[param_idx] + (1.0 - beta2) * gradients[param_idx] * gradients[param_idx];
 
-            double m_hat = m[offset + layer->num_inputs * layer->num_outputs + j] / (1 - pow(beta1, t));
-            double v_hat = v[offset + layer->num_inputs * layer->num_outputs + j] / (1 - pow(beta2, t));
+            double m_hat = m[param_idx] / (1.0 - pow(beta1, t));
+            double v_hat = v[param_idx] / (1.0 - pow(beta2, t));
 
             layer->biases[j] -= learning_rate * m_hat / (sqrt(v_hat) + epsilon);
+            param_idx++;
         }
     }
 }
