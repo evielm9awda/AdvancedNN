de3935118ca480859be6852476f88d51fa9ece05
EVIE Tipsy Doll <126894089+evielm9awda@users.noreply.github.com>
2024-07-10 11:00:14 -0400

    neural network computations

--- matrix_operations.h
+++ matrix_operations.h
@@ -1,8 +1,8 @@
 #ifndef MATRIX_OPERATIONS_H
 #define MATRIX_OPERATIONS_H
 
-void matrix_multiply(double* a, double* b, double* result, int a_rows, int a_cols, int b_cols);
-void matrix_add_bias(double* matrix, double* biases, double* result, int rows, int cols);
+void matrix_multiply(double* result, double* matrix1, int rows1, int cols1, double* matrix2, int rows2, int cols2);
+void matrix_add(double* result, double* matrix, double* biases, int rows, int cols);
 double dot_product(double* vec1, double* vec2, int length);
 double norm(double* vec, int length);
 
